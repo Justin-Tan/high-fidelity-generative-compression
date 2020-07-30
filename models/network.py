6a7d9ca32cc645631c3d7534411200eaf64031b5
Justin Tan <justan@student.unimelb.edu.au>
2020-07-31 02:20:52 +1000

    Johnnie Ray

--- models/network.py
+++ models/network.py
@@ -8,32 +8,40 @@ import functools
 from utils import normalization
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, kernel_size=3, stride=1, 
-                 norm_layer=None, activation='relu'):
-
+    def __init__(self, input_dims, kernel_size=3, stride=1, 
+                 channel_norm=True, activation='relu'):
+        """
+        input_dims: Dimension of input tensor (B,C,H,W)
+        """
         super(ResidualBlock, self).__init__()
 
         self.activation = getattr(F, activation)
-        self.norm_layer = norm_layer
-        if norm_layer is None:
-            self.norm_layer = nn.InstanceNorm2d(in_channels, affine=True,
-                                                track_running_stats=True)
+        in_channels = input_dims[1]
+        norm_kwargs = dict(momentum=0.1, affine=True, track_running_stats=True)
+
+        if channel_norm is True:
+            self.interlayer_norm = normalization.ChannelNorm2D_wrap
+        else:
+            self.interlayer_norm = normalization.InstanceNorm2D_wrap
 
         pad_size = int((kernel_size-1)/2)
         self.pad = nn.ReflectionPad2d(pad_size)
         self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride)
         self.conv2 = nn.Conv2d(in_channels, in_channels, kernel_size, stride=stride)
+        self.norm1 = self.interlayer_norm(input_dims, **norm_kwargs)
+        self.norm2 = self.interlayer_norm(input_dims, **norm_kwargs)
 
     def forward(self, x):
 
         identity_map = x
         res = self.pad(x)
         res = self.conv1(res)
-        res = self.activation(self.norm_layer(res))
+        res = self.norm1(res) 
+        res = self.activation(res)
 
         res = self.pad(res)
         res = self.conv2(res)
-        res = self.norm_layer(res)
+        res = self.norm2(res)
 
         return torch.add(res, identity_map)
 
@@ -202,8 +210,8 @@ class Generator(nn.Module):
         )
 
         for m in range(n_residual_blocks):
-            resblock_m = ResidualBlock(in_channels=filters[0], 
-                norm_layer=self.interlayer_norm, activation=activation)
+            resblock_m = ResidualBlock(input_dims=(batch_size, filters[0], H0, W0), 
+                channel_norm=channel_norm, activation=activation)
             self.add_module('resblock_{}'.format(str(m)), resblock_m)
         
         # TODO Transposed conv. alternative: https://distill.pub/2016/deconv-checkerboard/
@@ -267,7 +275,8 @@ class Discriminator(nn.Module):
         image_dims:     Dimensions of input image, (C_in,H,W)
         context_dims:   Dimensions of contextual information, (C_in', H', W')
         C:              Bottleneck depth, controls bits-per-pixel
-                        C = {2,4,8,16}
+                        C = {2,4,8,16}, C = C_in' if encoder output used
+                        as context.
 
         [1] Mentzer et. al., "High-Fidelity Generative Image Compression", 
             arXiv:2006.09965 (2020).
@@ -275,13 +284,14 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         
         self.image_dims = image_dims
-        self.context_dims = context_dim
+        self.context_dims = context_dims
         im_channels = self.image_dims[0]
         kernel_dim = 4
+        context_C_out = 12
         filters = (64, 128, 256, 512)
 
         # Upscale encoder output - (C, 16, 16) -> (12, 256, 256)
-        self.context_conv = nn.Conv2d(C, 12, kernel_size=3, padding=1, padding_mode='reflect')
+        self.context_conv = nn.Conv2d(C, context_C_out, kernel_size=3, padding=1, padding_mode='reflect')
         self.context_upsample = nn.Upsample(scale_factor=16, mode='nearest')
 
         # Images downscaled to 500 x 1000 + randomly cropped to 256 x 256
@@ -295,20 +305,20 @@ class Discriminator(nn.Module):
         if spectral_norm is True:
             norm = nn.utils.spectral_norm
         else:
-            norm = nn.Identity
+            norm = nn.utils.weight_norm
 
-        # (256,256) -> (256,256), with implicit padding
+        # (C_in + C_in', 256,256) -> (64,128,128), with implicit padding
         # TODO: Check if removing spectral norm in first layer works
-        self.conv1 = norm(nn.Conv2d(im_channels, filters[0], kernel_dim, **cnn_kwargs))
-
-        # (256,256) -> (128,128)
-        self.conv2 = norm(nn.Conv2d(filters[0], filters[1], kernel_dim, **cnn_kwargs))
+        self.conv1 = norm(nn.Conv2d(im_channels + context_C_out, filters[0], kernel_dim, **cnn_kwargs))
 
         # (128,128) -> (64,64)
-        self.conv3 = norm(nn.Conv2d(filters[1], filters[2], kernel_dim, **cnn_kwargs))
+        self.conv2 = norm(nn.Conv2d(filters[0], filters[1], kernel_dim, **cnn_kwargs))
 
         # (64,64) -> (32,32)
-        self.conv3 = norm(nn.Conv2d(filters[2], filters[3], kernel_dim, **cnn_kwargs))
+        self.conv3 = norm(nn.Conv2d(filters[1], filters[2], kernel_dim, **cnn_kwargs))
+
+        # (32,32) -> (16,16)
+        self.conv4 = norm(nn.Conv2d(filters[2], filters[3], kernel_dim, **cnn_kwargs))
 
         self.conv_out = nn.Conv2d(filters[3], 1, 1, stride=1)
 
@@ -319,7 +329,6 @@ class Discriminator(nn.Module):
         y = self.context_upsample(y)
 
         x = torch.cat((x,y), dim=1)
-        
         x = self.activation(self.conv1(x))
         x = self.activation(self.conv2(x))
         x = self.activation(self.conv3(x))
@@ -335,3 +344,4 @@ TEMPLATES
 Encoders / Decoders
 =====================
 """
+
