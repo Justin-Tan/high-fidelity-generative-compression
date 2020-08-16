fix lower bound gradients

src/helpers/maths.py
@@ -1,6 +1,30 @@
 import torch
 import numpy as np
 
+class LowerBoundIdentity(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor, lower_bound):
+        ctx.lower_bound = lower_bound
+        return torch.clamp(tensor, lower_bound)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.clone(), None
+
+
+class LowerBoundToward(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, tensor, lower_bound):
+        ctx.lower_bound = lower_bound
+        ctx.mask = tensor.ge(ctx.lower_bound)
+        return torch.clamp(tensor, lower_bound)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        gate = torch.logical_or(ctx.mask, grad_output.ge(0.)).type_as(grad_output.data)
+        gate.requires_grad = True
+        return grad_output * gate, None
+
 def gaussian_entropy(D, logvar):
     """
     Entropy of a Gaussian distribution with 'D' dimensions and heteroscedastic log variance 'logvar'
@@ -100,37 +124,3 @@ def matrix_log_density_gaussian(x, mu, logvar):
 
     return log_density_gaussian(x, mu, logvar)
 
-def log_importance_weight_matrix(batch_size, dataset_size):
-    """
-    Calculates a log importance weight matrix
-    Parameters
-    ----------
-    batch_size: int
-        number of training images in the batch
-    dataset_size: int
-        number of training images in the dataset
-
-    Returns:
-    log_W: Matrix of importance weights for estimating
-    log_qz, etc. according to (S6) of [1], repeat for
-    each x* (n*) in batch.
-    Shape [B,B]
-
-    [1]: https://arxiv.org/pdf/1802.04942.pdf
-    """
-    N = dataset_size
-    M = batch_size - 1
-    strat_weight = (N - M) / (N * M)
-    W = torch.Tensor(batch_size, batch_size).fill_(1 / M)
-    W.view(-1)[::M + 1] = 1 / N
-    W.view(-1)[1::M + 1] = strat_weight
-    W[M - 1, 0] = strat_weight
-    return W.log()
-
-def softmax(x, dim=-1):
-    e_x = torch.exp(x - x.max(dim=dim, keepdim=True)[0])
-    out = e_x / e_x.sum(dim=dim, keepdim=True)
-    return out
-
-def logit(x, eps=1e-6):
-    return torch.log(x + eps) - torch.log(1. - x + eps)

src/model.py
@@ -337,14 +337,13 @@ if __name__ == '__main__':
     logger.info("Number of trainable parameters: {}".format(utils.count_parameters(model)))
     logger.info("Estimated size: {} MB".format(utils.count_parameters(model) * 4. / 10**6))
 
-    logger.info('Starting forward pass ...')
-    start_time = time.time()
+    shape = [10, 3, 256, 256]
+    logger.info('Starting forward pass with input shape {}'.format(shape))
 
-    x = torch.randn([10, 3, 256, 256]).to(device)
+    start_time = time.time()
+    x = torch.randn(shape).to(device)
     losses = model(x)
     compression_loss, disc_loss = losses['compression'], losses['disc']
-    print('Compression loss shape', compression_loss.size())
-    print('Disc loss shape', disc_loss.size())
 
     logger.info('Delta t {:.3f}s'.format(time.time() - start_time))
 

src/network/hyperprior.py
@@ -20,6 +20,9 @@ HyperInfo = namedtuple(
     "bitstring side_bitstring",
 )
 
+lower_bound_toward = maths.LowerBoundIdentity.apply
+lower_bound_identity = maths.LowerBoundToward.apply
+
 class CodingModel(nn.Module):
     """
     Probability model for estimation of (cross)-entropies in the context
@@ -118,17 +121,25 @@ class PriorDensity(nn.Module):
 
     def likelihood(self, x, mean, scale):
 
-        scale = torch.clamp(scale, min=self.scale_lower_bound).float()
+        # scale = lower_bound_identity(scale, mself.scale_lower_bound)
+        scale = lower_bound_toward(scale, self.scale_lower_bound)
+
         # Assumes 1 - CDF(x) = CDF(-x)
         x = x - mean
         x = torch.abs(x)
         cdf_upper = self.standardized_CDF((0.5 - x) / scale)
         cdf_lower = self.standardized_CDF(-(0.5 + x) / scale)
+
+        # Naive
         # cdf_upper = self.standardized_CDF( (x - mean + 0.5) / scale )
         # cdf_lower = self.standardized_CDF( (x - mean - 0.5) / scale )
+
         likelihood_ = cdf_upper - cdf_lower
 
-        return torch.clamp(likelihood_, min=self.min_likelihood) # , max=self.max_likelihood)
+        # likelihood_ = lower_bound_identity(likelihood_, self.min_likelihood)
+        likelihood_ = lower_bound_toward(likelihood_, self.min_likelihood)
+
+        return likelihood_
 
     def forward(self, x, mean, scale):
         return self.likelihood(x, mean, scale)
@@ -223,14 +234,18 @@ class HyperpriorDensity(nn.Module):
         sign = sign.detach()
         likelihood_ = torch.abs(
             torch.sigmoid(sign * cdf_upper) - torch.sigmoid(sign * cdf_lower))
-        likelihood_ = torch.sigmoid(cdf_upper) - torch.sigmoid(cdf_lower)
+
+        # Naive
+        # likelihood_ = torch.sigmoid(cdf_upper) - torch.sigmoid(cdf_lower)
 
         # Reshape to (N,C,H,W)
         likelihood_ = torch.reshape(likelihood_, (C,N,H,W))
         likelihood_ = likelihood_.permute(1,0,2,3)
-        # print('LIKELIHOOD shape', likelihood.size())
 
-        return torch.clamp(likelihood_, min=self.min_likelihood)  #, max=self.max_likelihood)
+        # likelihood_ = lower_bound_identity(likelihood_, self.min_likelihood)
+        likelihood_ = lower_bound_toward(likelihood_, self.min_likelihood)
+
+        return likelihood_ #, max=self.max_likelihood)
 
     def forward(self, x, **kwargs):
         return self.likelihood(x)
@@ -342,10 +357,10 @@ class Hyperprior(CodingModel):
             side_bitstring=None, # TODO
         )
 
-        print(quantized_latents)
-        print(quantized_hyperlatents)
-        print(noisy_latents)
-        print(noisy_hyperlatents)
+        # print(quantized_latents)
+        # print(quantized_hyperlatents)
+        # print(noisy_latents)
+        # print(noisy_hyperlatents)
 
         return info
 